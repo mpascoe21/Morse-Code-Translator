86d872decffa74561cc3ee0aca7cc0c2f465440b
Maria <pascoe_maria@hotmail.com>
2022-03-24 16:51:13 +0000

    test file testing the functions

---
+++ functions.test.js
@@ -0,0 +1,35 @@
+// import { expect, test } from "@jest/globals";
+// import { describe } from "yargs";
+import { expect } from "@jest/globals";
+import { translateMorseToText, translateTextToMorse } from "./functions.js";
+
+describe("Test if text is being translated to Morse Code", () => {
+  test("Should return a string of '. -' ", () => {
+    expect(translateTextToMorse("Sos")).toBe("...  ---  ...");
+  });
+
+  test("Should be able to translate several words", () => {
+    expect(translateTextToMorse("he is")).toBe("....  .    ..  ...");
+  });
+
+  test("invalid input", () => {
+    expect(translateTextToMorse(".-")).toBe("invalid input");
+  });
+});
+
+describe("Test if Morse Code is being translated to text", () => {
+  test("Should return a string of letters", () => {
+    expect(translateMorseToText(".- -... -.-.")).toBe("abc");
+  });
+
+  test("Should return a string of numbers", () => {
+    expect(translateMorseToText(".---- ----- ---..")).toBe("108");
+  });
+
+  // NOT WORKING, THE CONDITION RUNS THE TEXT TO MORSE FUNCTION INSTEAD
+  // test("invalid input", () => {
+  //   expect(translateMorseToText("2 days")).toBe("invalid input");
+  // });
+});
+
+// ?? Why is it one test suite when there are two describes?
